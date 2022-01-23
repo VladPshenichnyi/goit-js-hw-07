53056630a4bafb8399a54ac21b61bd5a68d4ece7
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-23 17:13:23 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,117 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+galleryRef.innerHTML = createMarkup();
+galleryRef.addEventListener("click", onClickImage);
 
+let newInstance = "";
+let navButtons = "";
+let closeButton = "";
+let idx = 0;
+
+function createMarkup() {
+    let markup = "";
+    for (let i = 0; i < galleryItems.length; i += 1) {
+    const { preview, original, description } = galleryItems[i];
+    markup += `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        id="#${i}"
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>`;
+    }
+
+    return markup;
+}
+
+function getImageForView(number, step) {
+    const element = newInstance.element();
+    number += step;
+    if (number < 0) {
+    number = galleryItems.length - 1;
+    }
+    else if (number === galleryItems.length) {
+    number = 0;
+    }
+    element.firstElementChild.children[0].src = galleryItems[number].original;
+    element.firstElementChild.children[0].alt = galleryItems[number].description;
+
+    return number;
+}
+
+function lightboxButtonsAdd(instance) {
+    const elem = instance.element();
+    elem.innerHTML += `<button type="button" title="Close" class="basicLightbox__nav--close-btn">×</button>
+    <div class="basicLightbox__nav">
+        <button type="button" title="Previous" class="prev basicLightbox__nav--arrow">Previous</button>
+        <button type="button" title="Next" class="next basicLightbox__nav--arrow">Next</button>
+    </div>`;
+
+    navButtons = elem.querySelector(".basicLightbox__nav");
+    closeButton = elem.querySelector(".basicLightbox__nav--close-btn");
+}
+
+function lightboxListenersAdd() {
+    closeButton.addEventListener("click", onCloseButton);
+    navButtons.addEventListener("click", onClickButton);
+    window.addEventListener("keyup", onKeyPress);
+}
+
+function lightboxListenersRemove() {
+    closeButton.removeEventListener("click", onCloseButton);
+    navButtons.removeEventListener("click", onClickButton);
+    window.removeEventListener("keyup", onKeyPress);
+}
+
+function onClickButton(e) {
+    if (e.target.classList.contains("next")) {
+    idx = getImageForView(idx, 1);
+    }
+    else if (e.target.classList.contains("prev")) {
+    idx = getImageForView(idx, -1);
+    }
+}
+
+function onClickImage(e) {
+    e.preventDefault();
+    if (e.target.tagName === "IMG") {
+    idx = Number(e.target.attributes.id.value.substring(1));
+    newInstance = basicLightbox.create(`<img src="${galleryItems[idx].original}" alt="${galleryItems[idx].description}">`,{
+    onShow: (instance) => {
+        lightboxButtonsAdd(instance);
+        lightboxListenersAdd();
+        },
+        onClose: (instance) => {
+        lightboxListenersRemove();
+        },
+    });
+    newInstance.show();
+    }
+}
+
+function onCloseButton(e) {
+    newInstance.close();
+}
+
+function onKeyPress(event) {
+    switch (event.key) {
+    case "Esc":
+    case "Escape":
+    newInstance.close();
+    break;
+    case "Left":
+    case "ArrowLeft":
+    idx = getImageForView(idx, -1);
+    break;
+    case "Right":
+    case "ArrowRight":
+    idx = getImageForView(idx, 1);
+    break;
+    }
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+galleryRef.innerHTML = createMarkup();
 
+let lightbox = new SimpleLightbox(".gallery a", {});
+
+function createMarkup() {
+    let markup = "";
+    for (const { preview, original, description } of galleryItems) {
+    markup += `
+    <a class="gallery__item"  href="${original}"> 
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>`;
+    }
+
+    return markup;
+}
 console.log(galleryItems);
